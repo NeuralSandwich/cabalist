eeda2228a48c611947c6255e280f1563b7a324ec
Sean Jones <neuralsandwich@gmail.com>
2016-06-26 22:04:17 +0100

    Add skeleton files

---
+++ __init__.py
@@ -0,0 +1,4 @@
+import cabalist
+
+if __name__ == '__main__':
+   cab = cabalist.Cabalist()
---
+++ cabalist.py
@@ -0,0 +1,10 @@
+""" Cabalist """
+
+import os
+
+DEFAULT_CONFIG_PATH = "~/.cabconfig"
+
+class Cabalist(object):
+
+    def __init__(self):
+        print("Cabalist")
